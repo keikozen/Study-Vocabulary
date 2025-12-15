bc2f14cabdf91b3421482373c7248b1bd3627c81
keikozen <keiko.koosshi@gmail.com>
2025-12-14 23:40:22 -0800

    Script.js

--- Script.js
+++ Script.js
@@ -3,15 +3,3 @@ const flashcard = document.getElementById("flashcard");
 flashcard.addEventListener("click", () => {
   flashcard.classList.toggle("flipped");
 });
-
-// Quiz option logic
-const options = document.querySelectorAll(".quiz-option");
-options.forEach(option => {
-  option.addEventListener("click", () => {
-    if (option.textContent.includes("lucky discovery")) {
-      option.classList.add("correct");
-    } else {
-      option.classList.add("wrong");
-    }
-  });
-});
